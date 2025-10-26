ebc18198c0365b2674b085c0311cbd359671fc5c
Michau <botkotel@gmail.com>
2025-10-26 20:41:09 +0100

    - Moved Player logic to PlayerEnemyTest class. - Implemented switchable scenes for testing different game mechanics.

--- core/src/main/java/project/roguelike/RogueHammerGame.java
+++ core/src/main/java/project/roguelike/RogueHammerGame.java
@@ -4,36 +4,59 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import project.roguelike.entities.Player;
+import com.badlogic.gdx.Input;
 
 public class RogueHammerGame extends ApplicationAdapter {
 
     private SpriteBatch batch;
-    private Player player;
+    private Scene currentScene;
+    private Scene roomTest;
+    private Scene playerEnemyTest;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        player = new Player(Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f);
+
+        roomTest = new RoomTest();
+        playerEnemyTest = new PlayerEnemyTest();
+
+        currentScene = roomTest;
+        currentScene.create();
     }
 
     @Override
     public void render() {
-        float delta = Gdx.graphics.getDeltaTime();
-
-        player.update(delta);
+        handleInput();
 
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
-        player.render(batch);
+        currentScene.render(batch);
         batch.end();
+    }
+
+    private void handleInput() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.F1)) {
+            switchScene(roomTest);
+        } else if (Gdx.input.isKeyJustPressed(Input.Keys.F2)) {
+            switchScene(playerEnemyTest);
+        }
+    }
+
+    private void switchScene(Scene newScene) {
+        if (currentScene == newScene)
+            return;
+        if (currentScene != null)
+            currentScene.dispose();
+        currentScene = newScene;
+        currentScene.create();
     }
 
     @Override
     public void dispose() {
+        if (currentScene != null)
+            currentScene.dispose();
         batch.dispose();
-        player.dispose();
     }
 }
