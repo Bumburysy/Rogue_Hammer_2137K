ad38895b6ba19e278cd5e21a2ea46768f6a25322
Michau <botkotel@gmail.com>
2025-11-09 11:32:15 +0100

    Updated scene interface, added updated and resize functions.

--- core/src/main/java/project/roguelike/scenes/Scene.java
+++ core/src/main/java/project/roguelike/scenes/Scene.java
@@ -1,11 +1,15 @@
-package project.roguelike;
+package project.roguelike.scenes;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public interface Scene {
     void create();
 
+    void update(float delta);
+
     void render(SpriteBatch batch);
 
     void dispose();
+
+    void resize(int width, int height);
 }
