81a901e5890c25b1f3cffd529308fd70947cc4a3
Michau <botkotel@gmail.com>
2025-11-09 11:50:53 +0100

    Implemented NormalRoom class as subclass of Room. This will be used as stanadard room in game - functions for spawning enemies and items will be added soon.

---
+++ core/src/main/java/project/roguelike/rooms/NormalRoom.java
@@ -0,0 +1,10 @@
+package project.roguelike.rooms;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class NormalRoom extends Room {
+
+    public NormalRoom(Vector2 position, RoomShape shape) {
+        super(position, shape);
+    }
+}
