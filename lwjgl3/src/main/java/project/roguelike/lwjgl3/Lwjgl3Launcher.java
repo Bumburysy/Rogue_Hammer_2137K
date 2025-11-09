d5e891269c60f1fee5f8bf682d79c9478098dab4
Michau <botkotel@gmail.com>
2025-11-09 12:04:24 +0100

    Updated path for main application class.

--- lwjgl3/src/main/java/project/roguelike/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/project/roguelike/lwjgl3/Lwjgl3Launcher.java
@@ -2,7 +2,8 @@ package project.roguelike.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import project.roguelike.RogueHammerGame;
+
+import project.roguelike.core.RogueHammerGame;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
