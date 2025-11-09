99ae4ee0403ea12541833f3ac1a57c2ac3d49757
Michau <botkotel@gmail.com>
2025-11-09 11:39:20 +0100

    Simplified Bullet class removed directional textures, fixed bullet speed and rendering size.

--- core/src/main/java/project/roguelike/entities/Bullet.java
+++ core/src/main/java/project/roguelike/entities/Bullet.java
@@ -1,66 +1,49 @@
 package project.roguelike.entities;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import project.roguelike.core.GameConfig;
 
 public class Bullet {
-
-    private Texture textureUp;
-    private Texture textureDown;
-    private Texture textureLeft;
-    private Texture textureRight;
-    private Texture currentTexture;
-
+    private Texture texture;
     private Vector2 position;
     private Vector2 velocity;
-    private float speed = 400f;
+    private float speed = project.roguelike.core.GameConfig.BULLET_SPEED;
     private Rectangle bounds;
 
     public Bullet(float x, float y, Vector2 direction) {
-        textureUp = new Texture("textures/bullet_up.png");
-        textureDown = new Texture("textures/bullet_down.png");
-        textureLeft = new Texture("textures/bullet_left.png");
-        textureRight = new Texture("textures/bullet_right.png");
-
-        if (direction.x > 0)
-            currentTexture = textureRight;
-        else if (direction.x < 0)
-            currentTexture = textureLeft;
-        else if (direction.y > 0)
-            currentTexture = textureUp;
-        else if (direction.y < 0)
-            currentTexture = textureDown;
-
+        texture = new Texture("textures/bullet.png");
         position = new Vector2(x, y);
-        velocity = new Vector2(direction).nor().scl(speed);
-        bounds = new Rectangle(x, y, currentTexture.getWidth(), currentTexture.getHeight());
+        velocity = new Vector2(direction).nor();
+        bounds = new Rectangle(
+                x - GameConfig.BULLET_SIZE / 2f,
+                y - GameConfig.BULLET_SIZE / 2f,
+                GameConfig.BULLET_SIZE,
+                GameConfig.BULLET_SIZE);
     }
 
     public void update(float delta) {
-        position.add(velocity.x * delta, velocity.y * delta);
+        position.add(velocity.x * speed * delta, velocity.y * speed * delta);
         bounds.setPosition(position);
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(currentTexture, position.x, position.y);
+        batch.draw(texture, position.x - GameConfig.BULLET_SIZE / 2f,
+                position.y - GameConfig.BULLET_SIZE / 2f,
+                GameConfig.BULLET_SIZE, GameConfig.BULLET_SIZE);
     }
 
     public Rectangle getBounds() {
         return bounds;
     }
 
-    public boolean isOffScreen() {
-        return position.x < 0 || position.x > Gdx.graphics.getWidth()
-                || position.y < 0 || position.y > Gdx.graphics.getHeight();
+    public Vector2 getPosition() {
+        return position;
     }
 
     public void dispose() {
-        textureUp.dispose();
-        textureDown.dispose();
-        textureLeft.dispose();
-        textureRight.dispose();
+        texture.dispose();
     }
 }
