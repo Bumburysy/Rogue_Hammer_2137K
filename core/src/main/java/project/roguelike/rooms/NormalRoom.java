3533e339626c0c67e9f6e852c4d9cbbfb340e2d9
Michau <botkotel@gmail.com>
2025-11-18 18:18:02 +0100

    Updated NormalRoom - added enemies and items spawning functionality that utilizes new RoomContent Plan and Generator.

--- core/src/main/java/project/roguelike/rooms/NormalRoom.java
+++ core/src/main/java/project/roguelike/rooms/NormalRoom.java
@@ -1,10 +1,51 @@
 package project.roguelike.rooms;
 
 import com.badlogic.gdx.math.Vector2;
+import project.roguelike.core.RoomContentGenerator;
+import project.roguelike.core.RoomContentPlan;
+import java.util.Random;
 
 public class NormalRoom extends Room {
+    private static final Random random = new Random();
+    private static final int MIN_ENEMIES = 1;
+    private static final int MAX_ENEMIES = 5;
 
     public NormalRoom(Vector2 position, RoomShape shape) {
         super(position, shape);
+    }
+
+    @Override
+    public void generateContentIfNeeded() {
+        if (isContentGenerated()) {
+            return;
+        }
+
+        RoomContentPlan plan = createContentPlan();
+        RoomContentGenerator.generate(this, plan);
+        setContentGenerated(true);
+    }
+
+    private RoomContentPlan createContentPlan() {
+        RoomContentPlan plan = new RoomContentPlan();
+
+        addRandomEnemies(plan);
+        addStartingWeapon(plan);
+
+        return plan;
+    }
+
+    private void addRandomEnemies(RoomContentPlan plan) {
+        plan.minEnemies = MIN_ENEMIES;
+        plan.maxEnemies = MAX_ENEMIES;
+
+        int enemyCount = random.nextInt(MAX_ENEMIES - MIN_ENEMIES + 1) + MIN_ENEMIES;
+        for (int i = 0; i < enemyCount; i++) {
+            String type = random.nextBoolean() ? "goblin" : "orc";
+            plan.enemies.add(new RoomContentPlan.EnemySpawn(type, null));
+        }
+    }
+
+    private void addStartingWeapon(RoomContentPlan plan) {
+        plan.items.add(new RoomContentPlan.ItemSpawn("smg", null));
     }
 }
