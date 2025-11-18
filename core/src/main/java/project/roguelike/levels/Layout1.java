0bba25149b3d935f3782d2799096d56c5c3d2bdb
Michau <botkotel@gmail.com>
2025-11-18 18:15:27 +0100

    Updated laylouts to include boss room.

--- core/src/main/java/project/roguelike/levels/Layout1.java
+++ core/src/main/java/project/roguelike/levels/Layout1.java
@@ -4,12 +4,11 @@ import project.roguelike.rooms.Room.RoomShape;
 import project.roguelike.levels.RoomData.RoomType;
 
 public class Layout1 {
-
   public static final RoomData[][] layout = {
       { null, new RoomData(RoomType.START, RoomShape.D_SHAPE_S), null },
       { new RoomData(RoomType.NORMAL, RoomShape.L_SHAPE_E), new RoomData(RoomType.NORMAL, RoomShape.T_SHAPE_N),
           new RoomData(RoomType.NORMAL, RoomShape.L_SHAPE_S) },
       { new RoomData(RoomType.NORMAL, RoomShape.L_SHAPE_N), new RoomData(RoomType.NORMAL, RoomShape.I_SHAPE_E),
-          new RoomData(RoomType.NORMAL, RoomShape.L_SHAPE_W) }
+          new RoomData(RoomType.BOSS, RoomShape.L_SHAPE_W) }
   };
 };
--- core/src/main/java/project/roguelike/levels/Layout2.java
+++ core/src/main/java/project/roguelike/levels/Layout2.java
@@ -3,10 +3,9 @@ package project.roguelike.levels;
 import project.roguelike.levels.RoomData.RoomType;
 
 public class Layout2 {
-
         public static final RoomData[][] layout = {
                         { null, new RoomData(RoomType.START, null), null, new RoomData(RoomType.NORMAL, null) },
-                        { new RoomData(RoomType.NORMAL, null), new RoomData(RoomType.NORMAL, null), null,
+                        { new RoomData(RoomType.BOSS, null), new RoomData(RoomType.NORMAL, null), null,
                                         new RoomData(RoomType.NORMAL, null) },
                         { new RoomData(RoomType.NORMAL, null), new RoomData(RoomType.NORMAL, null),
                                         new RoomData(RoomType.NORMAL, null),
