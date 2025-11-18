eb0fee36b1967bd783b3c5a3979f1e34a702c571
Michau <botkotel@gmail.com>
2025-11-18 18:22:20 +0100

    Updated StartRoom - added decorational ladder and starting weapon.

--- core/src/main/java/project/roguelike/rooms/StartRoom.java
+++ core/src/main/java/project/roguelike/rooms/StartRoom.java
@@ -1,14 +1,85 @@
 package project.roguelike.rooms;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import project.roguelike.core.GameConfig;
+import project.roguelike.core.RoomContentGenerator;
+import project.roguelike.core.RoomContentPlan;
 
 public class StartRoom extends Room {
+    private static final float SPAWN_Y_OFFSET = GameConfig.TILE_SIZE;
+
+    private final Texture ladderTexture;
+    private final Vector2 ladderPosition;
+    private final float ladderWidth;
+    private final float ladderHeight;
+
     public StartRoom(Vector2 position, RoomShape shape) {
         super(position, shape);
+
+        this.ladderTexture = new Texture("textures/ladder.png");
+        this.ladderHeight = GameConfig.TILE_SIZE;
+        this.ladderWidth = calculateLadderWidth();
+        this.ladderPosition = calculateLadderPosition();
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        super.render(batch);
+        renderLadder(batch);
+    }
+
+    @Override
+    public void generateContentIfNeeded() {
+        if (isContentGenerated()) {
+            return;
+        }
+
+        RoomContentPlan plan = createStartingItems();
+        RoomContentGenerator.generate(this, plan);
+        setContentGenerated(true);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        if (ladderTexture != null) {
+            ladderTexture.dispose();
+        }
     }
 
     public Vector2 getSpawnPoint() {
-        return new Vector2(getPosition().x + GameConfig.ROOM_WIDTH / 2f, getPosition().y + GameConfig.ROOM_HEIGHT / 2f);
+        Vector2 pos = getPosition();
+        return new Vector2(
+                pos.x + GameConfig.ROOM_WIDTH / 2f,
+                pos.y + GameConfig.ROOM_HEIGHT / 2f - SPAWN_Y_OFFSET);
+    }
+
+    private float calculateLadderWidth() {
+        float aspectRatio = (float) ladderTexture.getWidth() / ladderTexture.getHeight();
+        return ladderHeight * aspectRatio;
+    }
+
+    private Vector2 calculateLadderPosition() {
+        Vector2 pos = getPosition();
+        return new Vector2(
+                pos.x + GameConfig.ROOM_WIDTH / 2f - ladderWidth / 2f,
+                pos.y + GameConfig.ROOM_HEIGHT / 2f - ladderHeight / 2f);
+    }
+
+    private void renderLadder(SpriteBatch batch) {
+        batch.draw(ladderTexture,
+                ladderPosition.x,
+                ladderPosition.y,
+                ladderWidth,
+                ladderHeight);
+    }
+
+    private RoomContentPlan createStartingItems() {
+        RoomContentPlan plan = new RoomContentPlan();
+        Vector2 pistolPos = new Vector2(11f, 3f);
+        plan.items.add(new RoomContentPlan.ItemSpawn("pistol", pistolPos));
+        return plan;
     }
 }
