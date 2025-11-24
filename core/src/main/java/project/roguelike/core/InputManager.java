561e026f961b2e8d97801d4e0a05734854779521
Michau <botkotel@gmail.com>
2025-11-24 11:17:45 +0100

    Updated InputManager - moved all input related code to this class, added rebindable key binds.

--- core/src/main/java/project/roguelike/core/InputManager.java
+++ core/src/main/java/project/roguelike/core/InputManager.java
@@ -6,15 +6,34 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.math.Vector2;
 
 public class InputManager extends InputAdapter {
-    private final WorldManager worldManager;
+    private final KeyBindings keyBindings;
     private final Vector2 moveDirection = new Vector2();
+
     private boolean shootPressed = false;
     private boolean shootJustPressed = false;
     private boolean reloadPressed = false;
-    private boolean pickupPressed = false;
+    private boolean usePressed = false;
+    private boolean useActiveItemPressed = false;
+    private boolean selectActiveItemPrevPressed = false;
+    private boolean selectActiveItemNextPressed = false;
 
-    public InputManager(WorldManager worldManager) {
-        this.worldManager = worldManager;
+    private ScrollCallback scrollCallback;
+
+    @FunctionalInterface
+    public interface ScrollCallback {
+        void onScroll(int amount);
+    }
+
+    public InputManager() {
+        this.keyBindings = KeyBindings.getInstance();
+    }
+
+    public InputManager(KeyBindings customBindings) {
+        this.keyBindings = customBindings;
+    }
+
+    public void setScrollCallback(ScrollCallback callback) {
+        this.scrollCallback = callback;
     }
 
     public void update() {
@@ -22,16 +41,45 @@ public class InputManager extends InputAdapter {
         updateActions();
     }
 
+    public InputState getState() {
+        return new InputState(
+                moveDirection,
+                shootPressed,
+                shootJustPressed,
+                reloadPressed,
+                usePressed,
+                useActiveItemPressed,
+                selectActiveItemPrevPressed,
+                selectActiveItemNextPressed);
+    }
+
+    public boolean isActionJustPressed(InputAction action) {
+        int key = keyBindings.getKey(action);
+        return key != -1 && Gdx.input.isKeyJustPressed(key);
+    }
+
+    public KeyBindings getKeyBindings() {
+        return keyBindings;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        if (scrollCallback != null) {
+            scrollCallback.onScroll((int) amountY);
+        }
+        return true;
+    }
+
     private void updateMovement() {
         moveDirection.setZero();
 
-        if (Gdx.input.isKeyPressed(Input.Keys.W))
+        if (isKeyPressed(InputAction.MOVE_UP))
             moveDirection.y += 1;
-        if (Gdx.input.isKeyPressed(Input.Keys.S))
+        if (isKeyPressed(InputAction.MOVE_DOWN))
             moveDirection.y -= 1;
-        if (Gdx.input.isKeyPressed(Input.Keys.A))
+        if (isKeyPressed(InputAction.MOVE_LEFT))
             moveDirection.x -= 1;
-        if (Gdx.input.isKeyPressed(Input.Keys.D))
+        if (isKeyPressed(InputAction.MOVE_RIGHT))
             moveDirection.x += 1;
 
         if (!moveDirection.isZero()) {
@@ -42,33 +90,20 @@ public class InputManager extends InputAdapter {
     private void updateActions() {
         shootPressed = Gdx.input.isButtonPressed(Input.Buttons.LEFT);
         shootJustPressed = Gdx.input.isButtonJustPressed(Input.Buttons.LEFT);
-        reloadPressed = Gdx.input.isKeyJustPressed(Input.Keys.R);
-        pickupPressed = Gdx.input.isKeyJustPressed(Input.Keys.E);
+        reloadPressed = isKeyJustPressed(InputAction.RELOAD);
+        usePressed = isKeyJustPressed(InputAction.USE);
+        useActiveItemPressed = isKeyJustPressed(InputAction.USE_ACTIVE_ITEM);
+        selectActiveItemPrevPressed = isKeyJustPressed(InputAction.SELECT_ACTIVE_ITEM_PREV);
+        selectActiveItemNextPressed = isKeyJustPressed(InputAction.SELECT_ACTIVE_ITEM_NEXT);
     }
 
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        worldManager.handleScroll((int) amountY);
-        return true;
+    private boolean isKeyPressed(InputAction action) {
+        int key = keyBindings.getKey(action);
+        return key != -1 && Gdx.input.isKeyPressed(key);
     }
 
-    public Vector2 getMoveDirection() {
-        return moveDirection;
-    }
-
-    public boolean isShootPressed() {
-        return shootPressed;
-    }
-
-    public boolean isShootJustPressed() {
-        return shootJustPressed;
-    }
-
-    public boolean isReloadPressed() {
-        return reloadPressed;
-    }
-
-    public boolean isPickupPressed() {
-        return pickupPressed;
+    private boolean isKeyJustPressed(InputAction action) {
+        int key = keyBindings.getKey(action);
+        return key != -1 && Gdx.input.isKeyJustPressed(key);
     }
 }
