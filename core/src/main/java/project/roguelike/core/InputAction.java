558ffeb897c61d954471b08ff82a40d905e12e6d
Michau <botkotel@gmail.com>
2025-11-24 11:15:21 +0100

    Implemented InputAction class to manage user input actions enums.

---
+++ core/src/main/java/project/roguelike/core/InputAction.java
@@ -0,0 +1,16 @@
+package project.roguelike.core;
+
+public enum InputAction {
+    MOVE_UP,
+    MOVE_DOWN,
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    SHOOT,
+    RELOAD,
+    USE,
+    USE_ACTIVE_ITEM,
+    SELECT_ACTIVE_ITEM_PREV,
+    SELECT_ACTIVE_ITEM_NEXT,
+    WEAPON_SCROLL,
+    PAUSE
+}
