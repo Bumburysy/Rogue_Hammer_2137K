86a854aa7fb32edee783168c1d25914a7ac015ed
Michau <botkotel@gmail.com>
2025-11-24 17:09:59 +0100

    Implemented Key item used to open chests.

---
+++ core/src/main/java/project/roguelike/items/currencyItems/Key.java
@@ -0,0 +1,22 @@
+package project.roguelike.items.currencyItems;
+
+import com.badlogic.gdx.graphics.Texture;
+import project.roguelike.entities.Player;
+
+public class Key extends CurrencyItem {
+    private static final String ITEM_ID = "key";
+    private static final String ITEM_NAME = "Key";
+    private static final int KEY_VALUE = 1;
+
+    public Key() {
+        super(ITEM_ID, ITEM_NAME, KEY_VALUE);
+        Texture spriteSheet = new Texture("textures/key.png");
+        initializeAnimation(spriteSheet);
+    }
+
+    @Override
+    public void onPickup(Player player) {
+        player.addKeys(value);
+        System.out.println("Picked up Key! Total keys: " + player.getKeys());
+    }
+}
