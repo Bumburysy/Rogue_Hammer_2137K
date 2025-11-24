ea8cecc0ec8e9d307b780abb8de4915bf827dcc1
Michau <botkotel@gmail.com>
2025-11-24 11:27:42 +0100

    Implemented MedKit item that can be picked up, added to inventory, and heals player when used.

---
+++ core/src/main/java/project/roguelike/items/activeItems/MedKit.java
@@ -0,0 +1,35 @@
+package project.roguelike.items.activeItems;
+
+import com.badlogic.gdx.graphics.Texture;
+import project.roguelike.core.GameConfig;
+import project.roguelike.entities.Player;
+
+public class MedKit extends ActiveItem {
+    private static final int HEAL_AMOUNT = 5;
+    private static final float COOLDOWN = 30f;
+
+    public MedKit() {
+        super("medkit", "Med Kit", COOLDOWN);
+        this.texture = new Texture("textures/medkit.png");
+        setTexture(texture, GameConfig.TILE_SIZE / 2f);
+    }
+
+    @Override
+    protected void onUse(Player player) {
+        int currentHealth = player.getHealth();
+        int maxHealth = player.getMaxHealth();
+
+        if (currentHealth < maxHealth) {
+            player.heal(HEAL_AMOUNT);
+            System.out.println("Used Med Kit! Healed " + HEAL_AMOUNT + " HP");
+        }
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        if (texture != null) {
+            texture.dispose();
+        }
+    }
+}
