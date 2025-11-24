721d552f012953127c81b900a6af64fb3aba5471
Michau <botkotel@gmail.com>
2025-11-24 17:08:38 +0100

    Implemeted Coin currency item used for buying items in shop rooms.

---
+++ core/src/main/java/project/roguelike/items/currencyItems/Coin.java
@@ -0,0 +1,22 @@
+package project.roguelike.items.currencyItems;
+
+import com.badlogic.gdx.graphics.Texture;
+import project.roguelike.entities.Player;
+
+public class Coin extends CurrencyItem {
+    private static final String ITEM_ID = "coin";
+    private static final String ITEM_NAME = "Coin";
+    private static final int COIN_VALUE = 1;
+
+    public Coin() {
+        super(ITEM_ID, ITEM_NAME, COIN_VALUE);
+        Texture spriteSheet = new Texture("textures/coin.png");
+        initializeAnimation(spriteSheet);
+    }
+
+    @Override
+    public void onPickup(Player player) {
+        player.addCoins(value);
+        System.out.println("Picked up Coin! Total coins: " + player.getCoins());
+    }
+}
