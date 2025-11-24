3dea8144334d4f978bf719f3e12bee1e940c45b1
Michau <botkotel@gmail.com>
2025-11-24 11:26:39 +0100

    Updated ActiveItem - changed bahavior to cooldown based usage.

--- core/src/main/java/project/roguelike/items/activeItems/ActiveItem.java
+++ core/src/main/java/project/roguelike/items/activeItems/ActiveItem.java
@@ -1,12 +1,46 @@
 package project.roguelike.items.activeItems;
 
+import project.roguelike.entities.Player;
 import project.roguelike.items.Item;
 
 public abstract class ActiveItem extends Item {
-    protected ActiveItem(String id, String name) {
+    protected float cooldown;
+    protected float currentCooldown;
+
+    protected ActiveItem(String id, String name, float cooldown) {
         super(id, name, ItemType.ACTIVE);
+        this.cooldown = cooldown;
+        this.currentCooldown = 0f;
     }
 
-    public void onUse() {
+    public void update(float delta) {
+        if (currentCooldown > 0) {
+            currentCooldown -= delta;
+            if (currentCooldown < 0) {
+                currentCooldown = 0;
+            }
+        }
+    }
+
+    public boolean canUse() {
+        return currentCooldown <= 0;
+    }
+
+    public void use(Player player) {
+        if (!canUse()) {
+            return;
+        }
+        onUse(player);
+        currentCooldown = cooldown;
+    }
+
+    protected abstract void onUse(Player player);
+
+    public float getCurrentCooldown() {
+        return currentCooldown;
+    }
+
+    public float getCooldown() {
+        return cooldown;
     }
 }
