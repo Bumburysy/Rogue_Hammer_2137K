5d827b36949fa212c1cd38553a8d956a67635d60
Michau <botkotel@gmail.com>
2025-11-24 12:55:38 +0100

    Updated SmallHealthPotion - added animations and removed unnecesary dispose method.

--- core/src/main/java/project/roguelike/items/consumableItems/SmallHealthPotion.java
+++ core/src/main/java/project/roguelike/items/consumableItems/SmallHealthPotion.java
@@ -1,17 +1,16 @@
 package project.roguelike.items.consumableItems;
 
 import com.badlogic.gdx.graphics.Texture;
-import project.roguelike.core.GameConfig;
 import project.roguelike.entities.Player;
 
 public class SmallHealthPotion extends ConsumableItem {
-    private static final int HEAL_AMOUNT = 3;
+    private static final int HEAL_AMOUNT = 1;
 
     public SmallHealthPotion() {
         super("smallHealthPotion", "Small Health Potion");
 
-        this.texture = new Texture("textures/potion_small.png");
-        setTexture(texture, GameConfig.TILE_SIZE / 2f);
+        Texture spriteSheet = new Texture("textures/potion_small.png");
+        initializeAnimation(spriteSheet);
     }
 
     @Override
@@ -24,14 +23,6 @@ public class SmallHealthPotion extends ConsumableItem {
             System.out.println("Consumed Small Health Potion! Healed " + HEAL_AMOUNT + " HP");
         } else {
             System.out.println("HP already full!");
-        }
-    }
-
-    @Override
-    public void dispose() {
-        super.dispose();
-        if (texture != null) {
-            texture.dispose();
         }
     }
 }
